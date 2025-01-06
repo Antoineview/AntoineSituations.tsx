8965811f57b03bee149e89e7b796b59e2b95ff89
Antoine <antoine.richard-capponi@epita.fr>
2025-01-06 15:46:53 +0100

    Update PostDate.tsx
    - Changed the date display from english to french !

--- components/PostDate.tsx
+++ components/PostDate.tsx
@@ -1,8 +1,18 @@
+// AntoineSituations.tsx/components/PostDate.tsx:1-12
 import { format, parseISO } from 'date-fns'
+import { fr } from 'date-fns/locale' // Import the French locale
 
-export default function PostDate({ dateString }: { dateString: string }) {
+interface PostDateProps {
+  dateString: string
+}
+
+export default function PostDate({ dateString }: PostDateProps) {
   if (!dateString) return null
 
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  return (
+    <time dateTime={dateString}>
+      {format(date, 'd MMMM yyyy', { locale: fr })}
+    </time>
+  )
 }
