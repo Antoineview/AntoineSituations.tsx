5a8f10af35b01bb49cd9358b108a38710e4a1f73
Antoine <antoine.richard-capponi@epita.fr>
2025-05-30 00:52:45 +0200

    added studio shortcut

--- components/BlogHeader.tsx
+++ components/BlogHeader.tsx
@@ -13,7 +13,9 @@ export default function BlogHeader({
   return (
     <div className="mb-7 mt-7">
       <h1 className="w-full font-normal tracking-tighter main-title block text-center md:text-left text-[clamp(3rem,min(12vw,6rem),6rem)] md:text-[clamp(2.5rem,min(12vw,7rem),7rem)]">
-        {title}
+        <Link href="/studio" className="hover:opacity-80 transition-opacity font-['Crozet-Regular']">
+          {title}
+        </Link>
       </h1>
       {lilparagraph && (
         <h1 className="mt-5 text-center text-base sm:text-lg md:text-left blog-subtitle">
