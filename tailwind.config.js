fb60d13fcb7fd00d9d4767c58306b52c287e9ea1
Antoine <antoine.richard-capponi@epita.fr>
2025-05-30 01:43:10 +0200

    new moving separator

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,15 @@ module.exports = {
         small: '0 5px 10px rgba(0, 0, 0, 0.12)',
         medium: '0 8px 30px rgba(0, 0, 0, 0.12)',
       },
+      animation: {
+        'scroll': 'scroll 20s linear infinite',
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
     },
   },
   plugins: [],
